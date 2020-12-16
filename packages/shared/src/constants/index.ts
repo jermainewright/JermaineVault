bcd318b63ece50dbd50723c60221e3753a77b7be
jermainewright <verifiedbosco@gmail.com>
2020-12-16 15:27:00 +0100

    Shared constants

---
+++ packages/shared/src/constants/index.ts
@@ -0,0 +1,7 @@
+export const SOCKET_EVENTS = {
+  CONNECTED: 'platform.connected',
+  METRIC_UPDATE: 'metric.update',
+  ALERT_TRIGGERED: 'alert.triggered'
+} as const;
+
+export const DEFAULT_PAGINATION_LIMIT = 25;
